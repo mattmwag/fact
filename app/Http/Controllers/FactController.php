e99d9be7769f9c67fb5035cf91e074579cb9dc87
Matthew Marcus <matthew.marcus@wagwalking.com>
2019-07-13 21:59:56 -0700

    Adds FactController

---
+++ app/Http/Controllers/FactController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FactController extends Controller
+{
+    public function hello()
+    {
+	return "Hello!";
+    }
+}
